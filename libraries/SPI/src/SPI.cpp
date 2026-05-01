c8f8eb1e967171ede3a6d69c56ad4e082da12da8
NikkiJ314 <nikujadv373@gmail.com>
2026-05-01 02:43:44 -0700

    hi

--- libraries/SPI/src/SPI.cpp
+++ libraries/SPI/src/SPI.cpp
@@ -114,9 +114,9 @@ void SPIClass::begin(int8_t sck, int8_t miso, int8_t mosi, int8_t ss)
         _ss = ss;
     }
 
-    spiAttachSCK(_spi, _sck);
-    spiAttachMISO(_spi, _miso);
-    spiAttachMOSI(_spi, _mosi);
+    // spiAttachSCK(_spi, _sck);
+    // spiAttachMISO(_spi, _miso);
+    // spiAttachMOSI(_spi, _mosi);
 
 }
 
