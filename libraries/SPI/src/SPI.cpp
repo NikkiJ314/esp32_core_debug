21c1fe52d751693831bc7f5194e0298a212abc00
NikkiJ314 <nikujadv373@gmail.com>
2026-05-01 02:03:27 -0700

    ji

--- libraries/SPI/src/SPI.cpp
+++ libraries/SPI/src/SPI.cpp
@@ -107,7 +107,7 @@ void SPIClass::begin(int8_t sck, int8_t miso, int8_t mosi, int8_t ss)
         _ss = ss;
     }
 
-    spiAttachSCK(_spi, _sck);
+    // spiAttachSCK(_spi, _sck);
     // int check = spiAttachMISO(_spi, _miso);
     // if (check == 1) {
     //     Serial.println("wattesigma");
